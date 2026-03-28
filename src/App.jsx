f846c83816ab0f8939c6c679cf3a8577f9bef658
Ouassim Babakhali <babakhaliouassim@gmail.com>
2026-03-28 03:01:36 +0100

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CustomCursor from './components/CustomCursor';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About';
 import Projects from './components/Projects';
 import Gallery from './components/Gallery';
 import Contact from './components/Contact';
@@ -19,6 +20,7 @@ function App() {
       <main>
         <Hero />
         
+        <About />
         {/* The Project Archive Slider */}
         <Projects />
         
